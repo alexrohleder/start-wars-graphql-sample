3364b3df2fef15fffabe81cf305031991824d7b4
Alex Rohleder <a.rohleder@sportradar.com>
2022-03-08 13:49:16 +0100

    working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,51 @@
 import type { NextPage } from "next";
+import { gql } from "@apollo/client";
+import client from "../lib/apollo-client";
 
-const Home: NextPage = () => {
-  return <div className="bg-red-100">...</div>;
+type Props = {
+  allPeople: any;
 };
+
+const Home: NextPage<Props> = (props) => {
+  return (
+    <pre className="bg-red-100">{JSON.stringify(props.allPeople, null, 4)}</pre>
+  );
+};
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      {
+        allPeople(first: 5) {
+          edges {
+            node {
+              name
+              hairColor
+              skinColor
+              eyeColor
+              gender
+              homeworld {
+                name
+              }
+            }
+            cursor
+          }
+          pageInfo {
+            startCursor
+            endCursor
+            hasPreviousPage
+            hasNextPage
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      allPeople: data.allPeople,
+    },
+  };
+}
 
 export default Home;
